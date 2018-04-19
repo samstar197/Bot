6cfa1196e86158b366ecbe779c006ce8db9bac55
samstar197 <38186229+samstar197@users.noreply.github.com>
2018-04-19 19:13:40 +0100

    Update BlobSpam.py

--- BlobSpam.py
+++ BlobSpam.py
@@ -30,10 +30,10 @@ async def on_message(message):
             val = 25
         for i in range(val):
             time.sleep(1)
-            if userID == 207963869895852032:
+            if userID == '207963869895852032':
                 await client.send_message(message.channel, 'Oh its only you' )
                 await client.send_message(message.channel, blobs[16])
-            elif userID == 418599237501059072:
+            elif userID == '418599237501059072':
                 await client.send_message(message.channel, 'You have made a grave enemy' )
                 await client.send_message(message.channel, blobs[1])
             else:
